578673af152548ed48abaf536f8cf9bdafaadcd4
Fhekz <fm.aldeco@gmail.com>
2023-07-17 11:57:04 -0300

    initial config 2

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-center">VIFIT24</h1>
+    </div>
+  );
+}
+
+export default App;
